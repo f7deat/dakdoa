acc9fc2f15f56909c381d10aba5e85bc1023fc30
f7deat <tan.dct9x@gmail.com>
2023-12-16 16:40:09 +0700

    amenities

--- src/components/card/image-overlays.tsx
+++ src/components/card/image-overlays.tsx
@@ -2,13 +2,14 @@ import overlays from '../../assets/css/overlays.svg';
 
 type CardImageOverlaysProps = {
     image: string;
+    maxHeight: number;
 }
 
 const CardImageOverlays: React.FC<CardImageOverlaysProps> = (props) => {
     return (
         <div className="relative text-white">
             <div className='absolute top-0 left-0 bottom-0 right-0 hover:bg-[#2CAF58E5] cursor-pointer z-10'></div>
-            <img src={props.image} alt="IMG" />
+            <img src={props.image} alt="IMG" height={props.maxHeight} />
             <div className="absolute bottom-0 px-8 py-6 bg-contain" style={{
                 backgroundImage: `url(${overlays})`
             }}>
