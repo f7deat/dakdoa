da9f01482c01c16b12507a0a27211f8fe44834cf
f7deat <f7deat@gmail.com>
2024-01-27 21:55:03 +0700

    change endpoint

--- src/services/request.tsx
+++ src/services/request.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'https://localhost:64452/api/',
+    baseURL: 'https://shinecgialai.com.vn/api/',
     headers: {
         Accept: 'application/json',
         'content-type': 'application/json',
