df98ddc21b20b944ba756ba3796a02d3ecf57e63
f7deat <f7deat@gmail.com>
2024-09-25 18:46:17 +0700

    infra

--- src/components/homes/index.ts
+++ src/components/homes/index.ts
@@ -4,5 +4,4 @@ export { default as Page2 } from './page2';
 export { default as News } from './news';
 export { default as Page4 } from './page4';
 export { default as Internal } from './internal';
-export { default as External } from './external';
 export { default as Testimonial } from './testimonial';
--- src/components/homes/internal.tsx
+++ src/components/homes/internal.tsx
@@ -1,57 +1,32 @@
-import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-import { useEffect, useRef, useState } from 'react';
 import { FormattedMessage, useIntl } from 'umi';
 import Header1 from '../header1';
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons';
-import { apiCatalogList } from '@/services/catalog';
+import gt from '../../assets/amenities/gt.png';
+import water from '../../assets/amenities/nuoc.png';
+import rac from '../../assets/amenities/rac.png';
+import fire from '../../assets/amenities/fire.png';
+import elec from '../../assets/amenities/elec.png';
+import inter from '../../assets/amenities/inter.png';
+
 
 const Internal: React.FC = () => {
 
-    const [activeIndex, setActiveIndex] = useState<number>(0);
-    const swiperRef = useRef<SwiperRef>(null);
-    const intl = useIntl();
-    const [dataSource, setDataSource] = useState<API.Catalog[]>([]);
-
-    useEffect(() => {
-        let parentId = '9eef307d-751e-44c7-3d31-08dc176ae07c';
-        if (intl.locale === 'en-US') {
-            parentId = '36c28881-24bb-40b2-3d37-08dc176ae07c';
-        }
-        if (intl.locale === 'zh-CN') {
-            parentId = '3428200a-edfd-4d16-0387-08dc376f0897';
-        }
-        if (intl.locale === 'ko-KR') {
-            parentId = 'c45c0761-8553-46e3-f2e2-08dc376fd91d';
-        }
-        if (intl.locale === 'ja-JP') {
-            parentId = '7a420f08-13bc-4db4-5019-08dc377012fa';
-        }
-        apiCatalogList({
-            parentId: parentId
-        }).then(response => {
-            setDataSource(response.data.data);
-        })
-    }, []);
-
-    const Amenities = (image: string, title: string, description: string[]) => (
-        <div className="md:px-4 text-green-700">
-            <figure className="snip1577">
-                <img src={image} className="w-full bg-cover h-[200px] 2xl:h-[250px]" alt="Amenities" loading="lazy" />
-                <figcaption>
-                    <h3>CCN SỐ 2 Đak Đoa</h3>
-                    <h4>Shinec Gia Lai</h4>
-                </figcaption>
-                <a href="#"></a>
-            </figure>
-            <div className="font-bold text-xl py-2 ">{title}</div>
-            <ul className="text-sm md:text-base list-disc" >
-                {
-                    description.map((value: string, index: number) => (
-                        <li key={index} className="ml-4">{value}</li>
-                    ))
-                }
-            </ul>
+    const Amenities = (image: string, title: string, description: string[], delay: number) => (
+        <div className="md:px-4 flex gap-4 mb-4"
+            data-aos="fade-up"
+            data-aos-delay={delay}>
+            <div>
+                <img src={image} />
+            </div>
+            <div className='flex-1'>
+                <div className="font-bold text-xl mb-1 text-blue-800">{title}</div>
+                <ul className="text-sm md:text-base list-disc text-blue-900" >
+                    {
+                        description.map((value: string, index: number) => (
+                            <li key={index} className="ml-4">{value}</li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     )
 
@@ -60,73 +35,48 @@ const Internal: React.FC = () => {
             <div className="h-full flex flex-col justify-between w-full" >
                 <div className="relative h-full">
                     <div className="flex h-full items-center md:py-10 py-8">
-                        <div className="container mx-auto px-10 md:px-0">
+                        <div className="container mx-auto px-4 md:px-0">
                             <Header1 active primary>
                                 Hạ tầng kỹ thuật đồng bộ và hiện đại
                             </Header1>
                             <div className="text-center mx-auto mb-8 text-base md:text-lg text-green-700">
                                 <FormattedMessage id='AMENITIES_DES' />
                             </div>
-                            <Swiper
-                                autoplay={{
-                                    pauseOnMouseEnter: true
-                                }}
-                                loop
-                                modules={[Autoplay]}
-                                ref={swiperRef}
-                                slidesPerView={1}
-                                onAutoplay={(event) => {
-                                    setActiveIndex(event.realIndex);
-                                }}
-                                breakpoints={{
-                                    768: {
-                                        slidesPerView: 1,
-                                    },
-                                    1024: {
-                                        slidesPerView: 2,
-                                    },
-                                    1280: {
-                                        slidesPerView: 3,
-                                    },
-                                    1536: {
-                                        slidesPerView: 4,
-                                    },
-                                }}
-                            >
-                                {
-                                    dataSource.map(item => (
-                                        <SwiperSlide>
-                                            {Amenities(item.thumbnail, item.name, item.description.split(','))}
-                                        </SwiperSlide>
-                                    ))
-                                }
-                            </Swiper>
-
-                            <div className="flex justify-between md:w-2/3 mx-auto gap-4">
-                                <button
-                                    onClick={() => {
-                                        swiperRef.current?.swiper.slidePrev();
-                                        setActiveIndex(swiperRef.current?.swiper.realIndex || 0);
-                                    }}
-                                    className="h-10 w-10 md:h-12 md:w-12 2xl:h-14 2xl:w-14 bg-white opacity-50 rounded-full flex items-center justify-center hover:opacity-100 transition duration-500">
-                                    <ArrowLeftOutlined />
-                                </button>
-                                <div className="flex items-center justify-center gap-2">
-                                    {
-                                        dataSource.map((item, index) => (
-                                            <button key={item.id} onClick={() => { setActiveIndex(index); swiperRef.current?.swiper.slideTo(index) }}
-                                                className={`h-4 md:h-5 w-4 md:w-5 rounded-full border-2 border-green-500 ` + (index === activeIndex ? 'bg-blue-500' : '')}></button>
-                                        ))
-                                    }
-                                </div>
-                                <button
-                                    onClick={() => {
-                                        swiperRef.current?.swiper.slideNext();
-                                        setActiveIndex(swiperRef.current?.swiper.realIndex || 0);
-                                    }}
-                                    className="h-10 w-10 md:h-12 md:w-12 2xl:h-14 2xl:w-14 bg-white opacity-50 rounded-full flex items-center justify-center hover:opacity-100 transition duration-500">
-                                    <ArrowRightOutlined />
-                                </button>
+                            <div className='grid md:grid-cols-3 gap-4'>
+                                {Amenities(
+                                    gt,
+                                    'Giao thông nội bộ', [
+                                    'Diện tích long đường giao thông nội bộ: 15m'
+                                ], 500)}
+                                {Amenities(
+                                    water,
+                                    'Hệ thống cấp nước và xử lý nước thải', [
+                                    'Cấp nước công nghiệp: 30m3/ha/ngày đêm. Xây dựng trạm bơm tăng áp tại khu đất kỹ thuật, công xuất trạm bơm 270m3/h.',
+                                    'Chỉ tiêu phát sinh nước thải: >80% chỉ tiêu nước cấp'
+                                ], 600)}
+                                {Amenities(
+                                    rac,
+                                    'Chất thải rắn phát sinh', [
+                                    '>0,3 tấn/ha',
+                                    'Tỷ lệ thu gom xử lý chất thải rắn: 100%'
+                                ], 700)}
+                                {Amenities(
+                                    fire,
+                                    'Phòng cháy chữa cháy', [
+                                    'Mạng lưới cấp nước chữa cháy là mạng lưới kết hợp với mạng lưới cấp nước sản xuất'
+                                ], 800)}
+                                {Amenities(
+                                    elec,
+                                    'Hệ thống cấp điện', [
+                                    '16 máy biến áp hạ áp cho các nhà đầu tư thứ cấp',
+                                    '3 trạm biến áp cho khu vực dịch vụ, khu chiếu sáng và khu kỹ thuật'
+                                ], 900)}
+                                {Amenities(
+                                    inter,
+                                    'Viễn thông liên lạc', [
+                                    '5 trạm BTS phủ song toàn bộ khu vực',
+                                    'Áp dụng công nghệ truy cập mạng quang thụ động theo tiêu chuẩn GPON – mạng quang thụ động tốc độ Gigabit'
+                                ], 1000)}
                             </div>
                         </div>
                     </div>
